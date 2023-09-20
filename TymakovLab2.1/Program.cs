a464e40fb22d4357ee0744ce478f680b06c899a4
ilya <121713022+rokokol@users.noreply.github.com>
2023-09-20 17:56:57 +0300

    Tymakov 2.1

---
+++ TymakovLab2.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TymakovLab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app asks your name and then welcome you");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLi kne($"Welcome, dear {name}!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
